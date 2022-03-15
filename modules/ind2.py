7e61e757d32a8a2fc33e1d6212a8c2a37646a7d6
ProkDmit <dmidtriypdv@gmail.com>
2022-03-15 20:43:58 +0300

    2 индивидуальное задание

---
+++ modules/ind2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+if __name__ == '__main__':
+    s=input("Введите предложение: ")
+    r=s.replace('про', 'нет')
+    print(f"Предложение после замены: {r}")
